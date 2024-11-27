2e38f87a350fd1b0b74535b5f5fe6541aba2e512
KumarGourav07 <iamkumargourav777@gmail.com>
2024-11-27 21:21:42 +0530

    push-changes

--- src/main/java/com/lnctproject/Placeservlet.java
+++ src/main/java/com/lnctproject/Placeservlet.java
@@ -16,6 +16,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+
 import com.google.gson.Gson;
 
 @WebServlet("/Placeservlet")
